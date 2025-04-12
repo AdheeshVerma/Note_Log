4db9a29db7a6bbb207939a25d8ad525d7b545022
adheeshverma <adheeshzkp@gmail.com>
2025-04-12 17:29:24 +0530

    back to render

--- notelogger/settings.py
+++ notelogger/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'notelogger.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
     
     }
 
